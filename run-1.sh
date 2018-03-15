5b34004bf5131f18e7d7bea8db37e72f1255b492
Ricarte Venerayan <venerayan@gmail.com>
2018-03-15 15:44:32 -0400

    Update run-1.sh

--- run-1.sh
+++ run-1.sh
@@ -6,7 +6,4 @@ if [[ ! -s x ]]; then
    sudo wget https://archive.fedoraproject.org/pub/epel/7Server/x86_64/Packages/e/epel-release-7-11.noarch.rpm
    sudo rpm -ivh epel-release-7-11.noarch.rpm
    sudo yum install git ansible sshpass -y
-else
-   echo ******There's no extra repository******
-   echo ******Install it first!******
 fi
