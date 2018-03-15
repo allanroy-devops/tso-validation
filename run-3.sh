a9312a8b1e8752f5473d690423d45daa96e64862
Ricarte Venerayan <venerayan@gmail.com>
2018-03-15 15:09:21 -0400

    Update run-3.sh

--- run-3.sh
+++ run-3.sh
@@ -9,7 +9,7 @@ chmod u+x create-loop.py
 ./create-loop.py input.csv
 mv server.yml roles/validate-linux/vars/
 ansible-playbook -i linux validate-linux.yml
-awk '{print "cp /tmp/"$1"_fetched/"$1"/tmp/"$1"_output.csv ."}' hosts > cp-csv
+awk '{print "cp /tmp/"$1"_fetched/"$1"/tmp/"$1"_output.csv ."}' linux > cp-csv
 chmod u+x  cp-csv
 ./cp-csv
 chmod u+x  validate.py
