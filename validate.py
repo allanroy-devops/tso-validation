a6c5ace0db35cd916d2f749466e08c1ca258d56c
Ricarte Venerayan <venerayan@gmail.com>
2018-03-16 15:15:02 -0400

    Update validate.py

--- validate.py
+++ validate.py
@@ -8,21 +8,21 @@ import sys
 #import argparse
 
 def _init_vars_input():
-    host_input = ""
-    ipaddress_input = ""
-    cpucore_input = ""
-    ram_input = ""
-    disk_input = ""
-    fact_file = ""
-    msg = ""
+    host_input = ''
+    ipaddress_input = ''
+    cpucore_input = ''
+    ram_input = ''
+    disk_input = ''
+    fact_file = ''
+    msg = ''
 
 def _init_vars_fact():
-    host_fact = ""
-    ipaddress_fact = ""
-    cpucore_fact = ""
-    ram_fact = ""
-    disk_fact = ""
-    msg = ""
+    host_fact = ''
+    ipaddress_fact = ''
+    cpucore_fact = ''
+    ram_fact = ''
+    disk_fact = ''
+    msg = ''
 
 if __name__ == '__main__':
     try:
@@ -47,18 +47,18 @@ if __name__ == '__main__':
             for row_input in reader_input:
                 #print(row_input['Hostname'], row_input['RemoteTestPort'])
                 #_init_vars_input()
-                host_input = ""
-                ipaddress_input = ""
-                cpucore_input = ""
-                ram_input = ""
-                disk_input = ""
-                fact_file = ""
-                msg = ""
+                host_input = ''
+                ipaddress_input = ''
+                cpucore_input = ''
+                ram_input = ''
+                disk_input = ''
+                fact_file = ''
+                msg = ''
 
                 host_input = row_input['Hostname'].replace(' ','')
                 if host_input:
                     fact_file = host_input + '_output.csv'
-                    msg = ""
+                    msg = ''
                     if os.path.isfile(fact_file):
                         ipaddress_input = row_input['IPAddress'].replace(' ','')
                         cpucore_input = row_input['CPUCore'].replace(' ','')
@@ -71,14 +71,14 @@ if __name__ == '__main__':
                             row_fact = None
                             for row_fact in reader_factfile:
                                 #_init_vars_fact()
-                                host_fact = ""
-                                checkhost_fact = ""
-                                port_fact = ""
-                                ipaddress_fact = ""
-                                cpucore_fact = ""
-                                ram_fact = ""
-                                disk_fact = ""
-                                msg = ""
+                                host_fact = ''
+                                checkhost_fact = ''
+                                port_fact = ''
+                                ipaddress_fact = ''
+                                cpucore_fact = ''
+                                ram_fact = ''
+                                disk_fact = ''
+                                msg = ''
 
                                 host_fact = row_fact['Hostname'].replace(' ','')
                                 checkhost_fact = row_fact['Check-Host'].replace(' ','')
@@ -89,42 +89,42 @@ if __name__ == '__main__':
                                 disk_fact = row_fact['Disk'].replace(' ','')
                                 if fact_row_ctr == 1:
                                     if (host_input != host_fact):
-                                        msg = "Host from input does not match value from fact file. Host input = " + host_input + ". Host fact = " + host_fact
+                                        msg = 'Host from input does not match value from fact file. Host input = ' + host_input + '. Host fact = ' + host_fact
                                     if (ipaddress_input != ipaddress_fact):
-                                        msg += "| IP address from input does not match value from fact file."
+                                        msg += '| IP address from input does not match value from fact file.'
                                     if (cpucore_input != cpucore_fact):
-                                        msg += "| No. of CPU cores from input does not match  value from fact file."
+                                        msg += '| No. of CPU cores from input does not match  value from fact file.'
                                     if (ram_input != ram_fact):
-                                        msg += "|RAM from input does not match value from fact file."
+                                        msg += '|RAM from input does not match value from fact file.'
                                     if (disk_input != disk_fact):
-                                        msg += "| Disk size from input does not match value from fact file."
+                                        msg += '| Disk size from input does not match value from fact file.'
                                     if not msg:
-                                        msg = "Passed"
+                                        msg = 'Passed'
                                 else:
                                     #_init_vars_input()
-                                    host_input = ""
-                                    ipaddress_input = ""
-                                    cpucore_input = ""
-                                    ram_input = ""
-                                    disk_input = ""
-                                    fact_file = ""
-                                    msg = ""
+                                    host_input = ''
+                                    ipaddress_input = ''
+                                    cpucore_input = ''
+                                    ram_input = ''
+                                    disk_input = ''
+                                    fact_file = ''
+                                    msg = ''
 
                                 output_dict = {'Hostname input': host_input, 'Hostname output': host_fact, 'Check-host': checkhost_fact, 'Port': port_fact, 'IP input': ipaddress_input, 'IP fact': ipaddress_fact, 'CPU core input': cpucore_input, 'CPU core fact': cpucore_fact, 'RAM input': ram_input, 'RAM fact': ram_fact, 'Disk input': disk_input, 'Disk fact': disk_fact, 'Message': msg}
                                 output_list.append(output_dict)
                                 fact_row_ctr += 1
                     else:
-                        msg += "Fact file " + fact_file + " not found."
-                        output_dict = {'Hostname input': host_input, 'Hostname output': "", 'Check-host': "", 'Port': "", 'IP input': "", 'IP fact': "", 'CPU core input': "", 'CPU core fact': "", 'RAM input': "", 'RAM fact': "", 'Disk input': "", 'Disk fact': "", 'Message': msg}
+                        msg += 'Fact file ' + fact_file + ' not found.'
+                        output_dict = {'Hostname input': host_input, 'Hostname output': '', 'Check-host': '', 'Port': '', 'IP input': '', 'IP fact': '', 'CPU core input': '', 'CPU core fact': '', 'RAM input': '', 'RAM fact': '', 'Disk input': '', 'Disk fact': '', 'Message': msg}
                         output_list.append(output_dict)
-        ###print (time.strftime("%d/%m/%Y"))
-        #output_file = "validation_" + time.strftime("%d%m%Y-%H%M") + ".csv"
-        output_file = "output.csv"
+        ###print (time.strftime('%d/%m/%Y'))
+        #output_file = 'validation_' + time.strftime('%d%m%Y-%H%M') + '.csv'
+        output_file = 'output.csv'
         with open(output_file, 'w') as csvfile_output:      
             fieldnames = ['Hostname input', 'Hostname output', 'Check-host', 'Port', 'IP input', 'IP fact', 'CPU core input', 'CPU core fact', 'RAM input', 'RAM fact', 'Disk input', 'Disk fact', 'Message']
             writer_output = csv.DictWriter(csvfile_output, fieldnames=fieldnames)
             writer_output.writeheader()
             writer_output.writerows(output_list)
     except:
-        print("Unexpected error:", sys.exc_info()[0])
+        print('Unexpected error:', sys.exc_info()[0])
         raise
