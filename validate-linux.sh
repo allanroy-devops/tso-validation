8a95d41dd36c56fc54074874bd7402f142caf03c
Ricarte Venerayan <venerayan@gmail.com>
2018-03-19 09:44:10 -0400

    Update validate-linux.sh

--- validate-linux.sh
+++ validate-linux.sh
@@ -6,6 +6,7 @@ awk -F',' 'FNR > 1 { print  $1 }' input.csv > temp
 #remove empty lines
 awk NF temp > linux
 chmod u+x create-loop.py
+chmod u+x validate.py
 ./create-loop.py input.csv
 mv server.yml roles/validate-linux/vars/
 ansible-playbook -i linux validate-linux.yml
